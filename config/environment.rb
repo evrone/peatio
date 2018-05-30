9edd29da56420548b63f265519e416dd0bfee56e
Andrey Voronkov <voronkovaa@gmail.com>
2018-05-30 17:41:20 +0300

    Fix for mysql2 adapter.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load mysql2_adapter fix
+require File.expand_path('../initializers/mysql2_adapter', __FILE__)
+
 # Initialize the rails application
 Peatio::Application.initialize!
